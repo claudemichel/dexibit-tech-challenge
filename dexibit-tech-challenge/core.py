96b4b8fcb3ee84d8ab8677d11d4e4871d92ce006
Administrator <admin@example.com>
2018-07-26 00:33:59 +0200

    Random decision tree. Exclude columns. 82% results

--- dexibit-tech-challenge/core.py
+++ dexibit-tech-challenge/core.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 
 #TODO: Create a constant or config file
@@ -9,9 +9,12 @@ exibition_train_file_path = '../ressources/anon-exhibition_train-input.csv'
 exibition_pred_file_path = '../ressources/anon-exhibition_pred-input.csv'
 #TODO: Find a more elegant way to get column name except exibition visitors (ex:  kaggle - handle missing value)
 
-exibition_predictors=['ExhibitionName', 'Date', 'DaysRun', 'DaysLeft', 'TotalDuration',
-       'PercentComplete', 'Year', 'Season', 'Month', 'WeekDay', 'ArtistName',
-       'CategoryName', 'LocationName', 'MarketingSpend', 'TicketPrice', 'VenueVisitors', 'ConversionRate']
+# exibition_predictors=['ExhibitionName', 'Date', 'DaysRun', 'DaysLeft', 'TotalDuration',
+#        'PercentComplete', 'Year', 'Season', 'Month', 'WeekDay', 'ArtistName',
+#        'CategoryName', 'LocationName', 'MarketingSpend', 'TicketPrice', 'VenueVisitors', 'ConversionRate']
+exibition_predictors=['Date',
+       'Season', 'WeekDay',
+       'CategoryName', 'LocationName', 'MarketingSpend', 'TicketPrice', 'VenueVisitors']
 exibition_predictors_to_drop = ['Date']
 
 #Def load data
@@ -23,19 +26,19 @@ def load_csv(file_path):
 
 
 def compute_dexibit_error_rate(val_y, val_predictions):
-    val_1, val_2 = np.array(val_y),np.array(val_predictions)
-    return np.mean(abs((val_2-val_1)/val_1))*100
+    val_y, val_predictions = np.array(val_y), np.array(val_predictions)
+    return (1 - np.mean(np.abs((val_y-val_predictions)/val_y)))*100
 
 
-def build_train_infer(max_leaf_nodes,train_X, train_y, val_X):
-    exibition_model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+def build_train_infer(train_X, train_y, val_X):
+    exibition_model = RandomForestRegressor()
     exibition_model.fit(train_X, train_y)
     val_predictions = exibition_model.predict(val_X)
     return val_predictions
 
 
-def get_error_rate(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    val_predictions = build_train_infer(max_leaf_nodes,train_X, train_y, val_X)
+def get_error_rate(train_X, val_X, train_y, val_y):
+    val_predictions = build_train_infer(train_X, train_y, val_X)
     return compute_dexibit_error_rate(val_y, val_predictions)
 
 
@@ -54,6 +57,5 @@ y = exibition_train_data.ExhibitionVisitors
 #Split data in two groups: validation process data and traininig process data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
 
-for max_leaf_nodes in [2,3,4,5, 6, 7, 8,9, 10, 30,40, 50 ,500]:
-    error_rate=get_error_rate(max_leaf_nodes, train_X, val_X, train_y, val_y)
-    print("Leaf nodes: %d \t Error rate:%d" % (max_leaf_nodes,error_rate))
+error_rate=get_error_rate(train_X, val_X, train_y, val_y)
+print("Error rate with :%d" % (error_rate))
