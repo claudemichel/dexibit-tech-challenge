3ae14ed72b2fd27040856a3fd152f9d5c25681c5
Administrator <admin@example.com>
2018-07-26 05:49:18 +0200

    XGBoost- Cross-validation evolution - Optimal value Plot 9=73%

--- dexibit-tech-challenge/core.py
+++ dexibit-tech-challenge/core.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_predict
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import KFold
 from xgboost import XGBRegressor
+from matplotlib import pyplot as plt
 
 #TODO: Create a constant or config file
 exibition_train_file_path = '../ressources/anon-exhibition_train-input.csv'
 exibition_pred_file_path = '../ressources/anon-exhibition_pred-input.csv'
-#TODO: Find a more elegant way to get column name except exibition visitors (ex:  kaggle - handle missing value)
 
+#TODO: Find a more elegant way to get column name except exibition visitors (ex:  kaggle - handle missing value)
 exibition_predictors=['ExhibitionName', 'Date', 'DaysRun', 'DaysLeft', 'TotalDuration',
        'PercentComplete', 'Year', 'Season', 'Month', 'WeekDay', 'ArtistName',
        'CategoryName', 'LocationName', 'MarketingSpend', 'TicketPrice', 'VenueVisitors', 'ConversionRate']
@@ -32,6 +34,10 @@ def compute_dexibit_error_rate(val_y, val_predictions):
     return (1 - np.mean(np.abs((val_y-val_predictions)/val_y)))*100
 
 
+def compute_score_from_mae(scores):
+    return -1*scores.mean()
+
+
 def build_train_infer(train_X, train_y, val_X, val_y):
     exibition_model = XGBRegressor(n_estimators=XGB_N_ESTIMATOR, learning_rate=XGB_LEARNING_RATE)
     exibition_model.fit(train_X, train_y, early_stopping_rounds=XGB_EARLY_STOPPING_ROUND, eval_set=[(val_X,val_y)])
@@ -40,9 +46,17 @@ def build_train_infer(train_X, train_y, val_X, val_y):
     return val_predictions
 
 
-def get_error_rate(train_X, val_X, train_y, val_y):
-    val_predictions = build_train_infer(train_X, train_y, val_X, val_y)
-    return compute_dexibit_error_rate(val_y, val_predictions)
+def get_error_rate(X,y,fold_number):
+    pipeline = make_pipeline(XGBRegressor())
+    cv = KFold(n_splits=fold_number,shuffle=False)
+    predictions = cross_val_predict(pipeline, X, y, cv=cv)
+    return compute_dexibit_error_rate(y,predictions)
+
+def display_cross_validation_plot():
+    plt.plot(fold_numbers, error_rate_evolution)
+    plt.xlabel("Cross-validation - Folds value")
+    plt.ylabel("Error rate (%)")
+    plt.show()
 
 
 exibition_train_data = load_csv(exibition_train_file_path)
@@ -55,9 +69,13 @@ X=pd.get_dummies(exibition_train_data[exibition_predictors].drop(exibition_predi
 #Define prediction target: 'ExhibitionVisitors'
 y = exibition_train_data.ExhibitionVisitors
 
-#Split data in two groups: validation process data and traininig process data
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+error_rate_evolution = list()
+fold_numbers=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-error_rate=get_error_rate(train_X, val_X, train_y, val_y)
-print("Error rate : %d%%" % (error_rate))
+for fold_number in fold_numbers:
+    error_rate=get_error_rate(X,y, fold_number)
+    print("Fold number %d - Error rate :%d"%(fold_number,error_rate))
+    error_rate_evolution.append(error_rate)
 
+
+display_cross_validation_plot()
